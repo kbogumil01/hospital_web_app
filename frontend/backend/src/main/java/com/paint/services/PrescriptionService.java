afbca3ee8d74a00fb6b09eb026c7afa06920577d
Karol <01158413@pw.edu.pl>
2023-05-21 14:24:33 +0200

    deleted useless backend folder in frontend dir

--- frontend/backend/src/main/java/com/paint/services/PrescriptionService.java
+++
@@ -1,8 +0,0 @@
-package com.paint.services;
-
-
-import com.paint.model.Prescription;
-
-public interface PrescriptionService {
-    Prescription addPrescriptionToPatient(Long id, Prescription prescription);
-}
